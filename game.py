839f311b03ad82b821d76aff2a2bb38919d71989
Dustin Flynn <flynnd297@yahoo.com>
2021-09-10 14:14:53 -0500

    Updated the welcome method within Game class to display the rules.

--- game.py
+++ game.py
@@ -1,13 +1,13 @@
 class Game:
     def __init__(self) -> None:
         self.rules = ''
+        
 
 
     def run_game(self):
         self.welcome()
 
-    def rules(self):
-        pass
-
+    
     def welcome(self):
-        print(f'Welcome to our game: Rock, Paper, Scissors, Lizard, Spock. \n Here are the rules {self.rules}')
+        print(f'Welcome to our game: Rock, Paper, Scissors, Lizard, Spock. \n Here are the rules: \n Rock crushes Scissors, Scissors cuts Paper, Paper covers Rock, Rock crushes Lizard, Lizard poisons Spock, Spock smashes Scissors, and Scissors decapitates Lizard. The game is best 2 out of 3')
+
