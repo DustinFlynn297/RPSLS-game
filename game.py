804c9334c3b51c7b6c1509947466ef855137543e
Dustin Flynn <flynnd297@yahoo.com>
2021-09-13 12:07:31 -0500

    Updated elif conditionals for player 2 gesture comparison to properly increment score. Updated playstyle method to check for invalid input. Created a blank validation method under game class.

--- game.py
+++ game.py
@@ -22,13 +22,16 @@ class Game:
         print(f'Welcome to our game: Rock, Paper, Scissors, Lizard, Spock. \n Here are the rules: \n Rock crushes Scissors, Scissors cuts Paper, Paper covers Rock, Rock crushes Lizard, Lizard poisons Spock, Spock smashes Scissors, and Scissors decapitates Lizard. The game is best 2 out of 3')
 
     def playstyle(self):
-        self.style = input('Would you like to play single player or multiplayer?').lower()
+        self.style = input('Would you like to play single player or multiplayer? ').lower()
         if (self.style == 'single player'):
             self.player1 = Human()
             self.player2 = AI()
-        else:
+        elif (self.stle == 'multiplayer'):
             self.player1 = Human()
             self.player2 = Human()
+        else:
+            print("Invalid input, please input a valid option")
+            self.playstyle()
 
     def display_selection(self):
         print(f"{self.player1.name} has chosen {self.player1.gesture} and {self.player2.name} has chosen {self.player2.gesture}")
@@ -46,35 +49,35 @@ class Game:
             self.display_selection()
             
         elif (self.player1.gesture == 'rock'):
-            if (self.player2.gesture == 'scissors'or 'lizard'):
+            if (self.player2.gesture == 'scissors'or self.player2.gesture == 'lizard'):
                 self.player1.score += 1
                 print("Player 1 wins round")
             else:
                 self.player2.score +=1
                 print("Player 2 wins round.")
         elif(self.player1.gesture == 'scissors'):
-            if(self.player2.gesture == 'paper' or 'lizard'):
+            if(self.player2.gesture == 'paper' or self.player2.gesture == 'lizard'):
                 self.player1.score += 1
                 print("Player 1 wins round")
             else:
                 self.player2.score +=1
                 print("Player 2 wins round.")
         elif(self.player1.gesture == 'paper'):
-            if(self.player2.gesture == 'rock' or 'spock'):
+            if(self.player2.gesture == 'rock' or self.player2.gesture == 'spock'):
                 self.player1.score += 1
                 print("Player 1 wins round")
             else:
                 self.player2.score +=1
                 print("Player 2 wins round.")
         elif(self.player1.gesture == 'lizard'):
-            if(self.player2.gesture == 'paper' or 'spock'):
+            if(self.player2.gesture == 'paper' or self.player2.gesture == 'spock'):
                 self.player1.score +=1
                 print("Player 1 wins round")
             else:
                 self.player2.score +=1
                 print("Player 2 wins round.")
         elif(self.player1.gesture == 'spock'):
-            if(self.player2.gesture == 'rock' or 'scissors'):
+            if(self.player2.gesture == 'rock' or self.player2.gesture == 'scissors'):
                 self.player1.score +=1
                 print("Player 1 wins round")
             else:
@@ -90,6 +93,9 @@ class Game:
             print(self.player2.name + " has won the match!")
         else:
             self.compare_gestures()
+
+    def gesture_validation(self):
+        pass
 
 
 
